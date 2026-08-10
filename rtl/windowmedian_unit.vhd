7c8c5a9dc0b20ef932590fde87806d1d012e4a8e
Kaan Karaali <kaankaraalipr@gmail.com>
2026-08-10 21:04:02 +0300

    Parameterize window median unit

--- rtl/windowmedian_unit.vhd
+++ rtl/windowmedian_unit.vhd
@@ -2,30 +2,33 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity windowmedian_unit is
+    generic (
+        G_PIXEL_WIDTH : positive := 8
+    );
     Port (
         clk          : in  STD_LOGIC;
         rst          : in  STD_LOGIC;
 
-        window_0     : in  STD_LOGIC_VECTOR(7 downto 0);
-        window_1     : in  STD_LOGIC_VECTOR(7 downto 0);
-        window_2     : in  STD_LOGIC_VECTOR(7 downto 0);
-        window_3     : in  STD_LOGIC_VECTOR(7 downto 0);
-        window_4     : in  STD_LOGIC_VECTOR(7 downto 0);
-        window_5     : in  STD_LOGIC_VECTOR(7 downto 0);
-        window_6     : in  STD_LOGIC_VECTOR(7 downto 0);
-        window_7     : in  STD_LOGIC_VECTOR(7 downto 0);
-        window_8     : in  STD_LOGIC_VECTOR(7 downto 0);
+        window_0     : in  STD_LOGIC_VECTOR(G_PIXEL_WIDTH-1 downto 0);
+        window_1     : in  STD_LOGIC_VECTOR(G_PIXEL_WIDTH-1 downto 0);
+        window_2     : in  STD_LOGIC_VECTOR(G_PIXEL_WIDTH-1 downto 0);
+        window_3     : in  STD_LOGIC_VECTOR(G_PIXEL_WIDTH-1 downto 0);
+        window_4     : in  STD_LOGIC_VECTOR(G_PIXEL_WIDTH-1 downto 0);
+        window_5     : in  STD_LOGIC_VECTOR(G_PIXEL_WIDTH-1 downto 0);
+        window_6     : in  STD_LOGIC_VECTOR(G_PIXEL_WIDTH-1 downto 0);
+        window_7     : in  STD_LOGIC_VECTOR(G_PIXEL_WIDTH-1 downto 0);
+        window_8     : in  STD_LOGIC_VECTOR(G_PIXEL_WIDTH-1 downto 0);
         window_valid : in  STD_LOGIC;
         window_ready : out STD_LOGIC;
 
-        median_out   : out STD_LOGIC_VECTOR(7 downto 0);
+        median_out   : out STD_LOGIC_VECTOR(G_PIXEL_WIDTH-1 downto 0);
         median_valid : out STD_LOGIC
     );
 end windowmedian_unit;
 
 architecture Behavioral of windowmedian_unit is
 
-    signal median_pixel_in_s    : STD_LOGIC_VECTOR(7 downto 0);
+    signal median_pixel_in_s    : STD_LOGIC_VECTOR(G_PIXEL_WIDTH-1 downto 0);
     signal median_pixel_valid_s : STD_LOGIC;
     signal median_pixel_ready_s : STD_LOGIC;
     signal median_valid_s       : STD_LOGIC;
@@ -35,6 +38,9 @@ begin
     median_valid <= median_valid_s;
 
     U_CONVERTER : entity work.paralel_to_series_converter
+        generic map (
+            G_PIXEL_WIDTH => G_PIXEL_WIDTH
+        )
         port map (
             clk                => clk,
             rst                => rst,
@@ -56,8 +62,11 @@ begin
         );
 
     U_MEDIAN : entity work.nine_input_median
+        generic map (
+            G_PIXEL_WIDTH => G_PIXEL_WIDTH
+        )
         port map (
-            CLK          => clk,
+            clk          => clk,
             rst          => rst,
             pixel_in     => median_pixel_in_s,
             pixel_valid  => median_pixel_valid_s,
